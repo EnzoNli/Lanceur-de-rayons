3de446d6cd508e0514052d70cb5e40290d5e4cce
EnzoNli <nulli.enzo@hotmail.com>
2023-01-25 22:13:59 +0100

    :rocket: Activation tests de comparateur

--- tests.sh
+++ tests.sh
@@ -15,15 +15,15 @@ set -e
 
 . assert.sh
 
-# echo "Tests du comparateur d'images"
+echo "Tests du comparateur d'images"
 
-# assert "./compare.sh TEST1/image1.png TEST1/image1.png" "OK\n0\n"
-# assert "./compare.sh TEST1/image2.png TEST1/image2.png" "OK\n0\n"
-# assert "./compare.sh TEST1/image1.png TEST1/image2.png" "OK\n879\n"
-# assert "cp diff.png localdiff.png"
-# assert "./compare.sh TEST1/diff.png localdiff.png" "OK\n0\n"
+assert "./compare.sh TEST1/image1.png TEST1/image1.png" "OK\n0\n"
+assert "./compare.sh TEST1/image2.png TEST1/image2.png" "OK\n0\n"
+assert "./compare.sh TEST1/image1.png TEST1/image2.png" "OK\n879\n"
+assert "cp diff.png localdiff.png"
+assert "./compare.sh TEST1/diff.png localdiff.png" "OK\n0\n"
 
-# assert_end regression
+assert_end regression
 
 # echo "Tests de la bibliothèque mathématique"
 
