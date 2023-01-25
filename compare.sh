26bac619eb410aaf9345f6b5fbdcbce1cd1068e4
EnzoNli <nulli.enzo@hotmail.com>
2023-01-25 21:44:07 +0100

    :construction: Implementation du comparateur

--- compare.sh
+++ compare.sh
@@ -5,5 +5,4 @@ IMAGE2=$2
 # Pour connaître l'emplacement du script compare.sh
 MYPATH=$(dirname "$0")
 
-# java -cp $MYPATH/bin CompareImage $IMAGE1 $IMAGE2
-echo "Pas encore réalisé"
+java -cp $MYPATH/bin CompareImage $IMAGE1 $IMAGE2
---
+++ src/CompareImage.java
@@ -0,0 +1,49 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class CompareImage{
+    public static void main(String[] args){
+        BufferedImage image1 = null;
+        BufferedImage image2 = null;
+        BufferedImage diff = null;
+        int compteur = 0;
+        try {
+            image1 = ImageIO.read(new File(args[0]));
+            image2 = ImageIO.read(new File(args[1]));
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(image1.getWidth() == image2.getWidth() && image1.getWidth() == image2.getWidth()){
+            diff = new BufferedImage(image1.getWidth(), image1.getHeight(), BufferedImage.TYPE_INT_RGB);
+            for(int i = 0; i < image1.getHeight(); i++) {
+                for(int j = 0; j < image1.getWidth(); j++) {
+                    if(image1.getRGB(i, j) != image2.getRGB(i, j)){
+                        compteur++;
+                        diff.setRGB(i, j, image1.getRGB(i, j));
+                    }else{
+                        diff.setRGB(i, j, 0);
+                    }
+                }
+            }
+            if(compteur >= 1){
+                try{
+                    ImageIO.write(diff, "png", new File("./diff.png"));
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            if(compteur <= 1000) {
+                System.out.println("OK");
+            } else {
+                System.out.println("KO");
+            }
+            System.out.println(compteur);
+        }
+    }
+}
