2332270e53f8f6d55bd1a1fdd40629b27bda30be
EnzoNli <nulli.enzo@hotmail.com>
2023-01-27 17:33:57 +0100

    :rewind: C'etait mieux avant !

--- src/CompareImage.java
+++ src/CompareImage.java
@@ -1,7 +1,7 @@
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
 
 public class CompareImage{
@@ -14,14 +14,14 @@ public class CompareImage{
             image1 = ImageIO.read(new File(args[0]));
             image2 = ImageIO.read(new File(args[1]));
             if(image1.getWidth() == image2.getWidth() && image1.getWidth() == image2.getWidth()){
-                diff = new BufferedImage(image1.getWidth(), image1.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
+                diff = new BufferedImage(image1.getWidth(), image1.getHeight(), BufferedImage.TYPE_INT_RGB);
                 for(int i = 0; i < image1.getWidth(); i++) {
                     for(int j = 0; j < image1.getHeight(); j++) {
                         if(image1.getRGB(i, j) != image2.getRGB(i, j)){
                             compteur++;
                             diff.setRGB(i, j, image1.getRGB(i, j));
                         }else{
-                            diff.setRGB(i, j, Color.black.getRGB());
+                            diff.setRGB(i, j, 0);
                         }
                     }
                 }
