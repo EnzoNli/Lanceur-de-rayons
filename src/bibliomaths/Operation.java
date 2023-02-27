d63a5430cb95005582e6ef54c93cef58051473aa
EnzoNli <nulli.enzo@hotmail.com>
2023-02-27 14:19:14 +0100

    :fire: v1666666666666666666666666666

--- src/bibliomaths/Operation.java
+++ src/bibliomaths/Operation.java
@@ -1,45 +1,44 @@
 package bibliomaths;
 
-import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
+import java.util.logging.*;
 
 public class Operation {
 
-    private static final Logger LOGGER = Logger.getLogger(Operation.class.getName());
+    private static final Logger LOGGER = Logger.getLogger("bug");
     private String rawData;
 
     /**
      * Le constructeur de la class Operation
+     * 
      * @param rawData correspond à la chaine de caractère à parser.
      */
-    public Operation(String rawData){
+    public Operation(String rawData) {
         this.rawData = rawData;
-        LOGGER.setLevel(Level.INFO);
-        try {
-            FileHandler fileHandler = new FileHandler("operation-parser.log");
-            fileHandler.setFormatter(new SimpleFormatter());
-            LOGGER.addHandler(fileHandler);
-            LOGGER.setLevel(Level.INFO);
-        } catch (IOException e) {
-            LOGGER.log(Level.WARNING, "Erreur lors de l'initialisation du gestionnaire de fichier", e);
-        }
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new SimpleFormatter() {
+            private static final String format = "%3$s %n";
+
+            @Override
+            public String formatMessage(LogRecord record) {
+                return record.getMessage();
+            }
+        });
+        LOGGER.setUseParentHandlers(false);
+        LOGGER.addHandler(handler);
     }
 
     /**
      * Cette méthode utilise les méthodes buildObject et display
      */
-    public void exec(){
+    public void exec() {
         String[] data = rawData.split(",");
         String operation = data[1];
-        Object o1 = buildObject(data[0]); 
+        Object o1 = buildObject(data[0]);
         Object o2 = buildObject(data[2]);
         try {
             Class<?> clazz2 = (o2.getClass() == Double.class) ? double.class : o2.getClass();
-            Object o3 = o1.getClass().getMethod(operation,clazz2).invoke(o1,o2);
-            //System.out.println(display(o3));
+            Object o3 = o1.getClass().getMethod(operation, clazz2).invoke(o1, o2);
+            // System.out.println(display(o3));
             LOGGER.info(display(o3));
         } catch (Exception e) {
             LOGGER.info("Interdit");
@@ -48,21 +47,22 @@ public class Operation {
 
     /**
      * Cette méthode permet de parser.
+     * 
      * @param s correspond à la chaine de carctère à parser
      * @return un Point, un Vector, une Couleur ou un Double.
      */
     private static Object buildObject(String s) {
         String[] tmp;
-        if(s.contains(" ")){
+        if (s.contains(" ")) {
             tmp = s.split(" ");
-            
+
             double x1 = Double.parseDouble(tmp[1]);
-    
+
             double y1 = Double.parseDouble(tmp[2]);
-    
+
             double z1 = Double.parseDouble(tmp[3]);
-    
-            switch(tmp[0]){
+
+            switch (tmp[0]) {
                 case "V":
                     return new Vector(x1, y1, z1);
                 case "P":
@@ -75,24 +75,26 @@ public class Operation {
         }
         return Double.parseDouble(s);
     }
-    
+
     /**
-     * Cette méthode permet de déterminer si l'objet passer en paramétre est une Couleur, un Point ou un Vector.
+     * Cette méthode permet de déterminer si l'objet passer en paramétre est une
+     * Couleur, un Point ou un Vector.
+     * 
      * @param o3 correspond à un Triplet.
      * @return la chaine de caractère correspondant à l'objet passé en paramètre.
      */
     private static String display(Object o3) {
-        if(o3.getClass() == Vector.class){
+        if (o3.getClass() == Vector.class) {
             Vector v = (Vector) o3;
             return v.toString();
         }
 
-        if(o3.getClass() == Point.class){
+        if (o3.getClass() == Point.class) {
             Point p = (Point) o3;
             return p.toString();
         }
 
-        if(o3.getClass() == Couleur.class){
+        if (o3.getClass() == Couleur.class) {
             Couleur c = (Couleur) o3;
             return c.toString();
         }
